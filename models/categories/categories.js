ede34b55459e272bfe23e54feb5314aaf00b01e7
Aliaksei Sauran <a.sauran@godeltech.com>
2018-03-01 11:07:39 +0300

    Just tag in categories schema

--- models/categories/categories.js
+++ models/categories/categories.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const categoriesSchema = new Schema({
   title: { type: String, required: true },
-  category_tag: String,
+  tag: { type: String, required: true },
 });
 
 const Categories = mongoose.model('Categories', categoriesSchema);
