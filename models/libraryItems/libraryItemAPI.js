398e79a5ca7bd49e4e80dd824783b6d06e2dd637
Aliaksei Sauran <a.sauran@godeltech.com>
2018-03-06 18:32:20 +0300

    Library items API and routes refactoring

--- models/libraryItems/libraryItemAPI.js
+++ models/libraryItems/libraryItemAPI.js
@@ -1,18 +1,13 @@
 const libraryItem = require('./libraryItem');
 
-const addOneItem = item => {
+const addItem = item => {
   const itemToAdd = new libraryItem(item);
   return itemToAdd.save();
 };
-const getAllItems = () => libraryItem.find({});
 
-const searchByType = typeName => libraryItem.find({ type: typeName });
-
-const searchItemsInCategory = searchQuiery => libraryItem.find(searchQuiery);
+const getItems = searchQuiery => libraryItem.find(searchQuiery);
 
 module.exports = {
-  addOneItem,
-  getAllItems,
-  searchByType,
-  searchItemsInCategory,
+  addItem,
+  getItems,
 };
--- routes/libraryRoutes.js
+++ routes/libraryRoutes.js
@@ -1,29 +1,18 @@
 const express = require('express');
 
 const router = express.Router();
-const {
-  addOneItem,
-  getAllItems,
-  searchByType,
-  searchItemsInCategory,
-} = require('../models/libraryItems/libraryItemAPI');
+const { addItem, getItems } = require('../models/libraryItems/libraryItemAPI');
 
 const libraryRoutes = router => {
   router
     .route('/library')
     .get((req, res) => {
-      if (req.query) {
-        searchItemsInCategory(req.query).then(items => {
-          res.json(items);
-        });
-      } else {
-        getAllItems().then(item => {
-          res.json(item);
-        });
-      }
+      getItems(req.query).then(items => {
+        res.json(items);
+      });
     })
     .post((req, res) => {
-      addOneItem(req.body).then(() => {
+      addItem(req.body).then(() => {
         res.json({
           message: 'Document was created successfully!',
         });
