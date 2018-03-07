f0d0fa01f8f2502caf162a441fee59fd34cdf274
Aliaksei Sauran <a.sauran@godeltech.com>
2018-03-07 18:34:42 +0300

    Validate query to libraryItems as strings without numbers, spaces and other characters

--- models/libraryItems/libraryItemAPI.js
+++ models/libraryItems/libraryItemAPI.js
@@ -1,11 +1,24 @@
 const libraryItem = require('./libraryItem');
+const validateAsString = require('./validateLibraryQuery');
 
 const addItem = item => {
   const itemToAdd = new libraryItem(item);
   return itemToAdd.save();
 };
 
-const getItems = searchQuiery => libraryItem.find(searchQuiery);
+const getItems = searchQuiery => {
+  const { categoryTag, type } = searchQuiery;
+  if (
+    categoryTag &&
+    validateAsString(categoryTag) &&
+    type &&
+    validateAsString(type)
+  ) {
+    return libraryItem.find({ categoryTag, type });
+  } else {
+    return Promise.reject(new Error('Invalid queries'));
+  }
+};
 
 module.exports = {
   addItem,
--- routes/libraryRoutes.js
+++ routes/libraryRoutes.js
@@ -7,9 +7,13 @@ const libraryRoutes = router => {
   router
     .route('/library')
     .get((req, res) => {
-      getItems(req.query).then(items => {
-        res.json(items);
-      });
+      getItems(req.query)
+        .then(items => {
+          res.json(items);
+        })
+        .catch(err => {
+          res.json(err.message);
+        });
     })
     .post((req, res) => {
       addItem(req.body).then(() => {
