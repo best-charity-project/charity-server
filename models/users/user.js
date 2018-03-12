4ac4c2d1203bc44357a7fdd5974d7f20c70d1980
Aliaksei Sauran <a.sauran@godeltech.com>
2018-03-12 18:12:49 +0300

    Create user schema

---
+++ models/users/user.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    select: false,
+  },
+  country: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  city: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  reasonForRegistration: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  passwordSalt: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  admin: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
