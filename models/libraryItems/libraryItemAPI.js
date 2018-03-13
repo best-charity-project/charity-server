fd2e9920866ba72f5f983551859221fb4eff87be
Olga Yakimovich <v.yakimovich@godeltech.com>
2018-03-13 18:45:12 +0300

    removed approved variable from getItems function

--- models/libraryItems/libraryItemAPI.js
+++ models/libraryItems/libraryItemAPI.js
@@ -7,22 +7,15 @@ const addItem = item => {
 };
 
 const getItems = searchQuery => {
-  const { categoryTag, type, approved } = searchQuery;
-  if (
-    categoryTag &&
-    isValidQuery(categoryTag) &&
-    type &&
-    isValidQuery(type) &&
-    approved
-  ) {
+  const { categoryTag, type } = searchQuery;
+  if (categoryTag && isValidQuery(categoryTag) && type && isValidQuery(type)) {
     return libraryItem.find({
       categoryTag,
       type,
       approved: true,
     });
-  } else {
-    return Promise.reject(new Error('Invalid queries'));
   }
+  return Promise.reject(new Error('Invalid queries'));
 };
 
 const getPendingItems = () => libraryItem.find({ approved: false });
