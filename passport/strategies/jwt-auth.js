ab66bc4ce4eb88e679416ce5498944a6cd2fd3ad
Aliaksei Sauran <a.sauran@godeltech.com>
2018-03-14 17:59:36 +0300

    Create strategy for checking user's auth status

---
+++ passport/strategies/jwt-auth.js
@@ -0,0 +1,15 @@
+const passport = require('passport');
+const userAPI = require('../../models/users/userAPI');
+const jwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const { jwtSecret } = require('../../configs/config');
+
+const opts = {};
+opts.secretOrKey = jwtSecret;
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.ignoreExpiration = true;
+
+module.exports = new jwtStrategy(opts, (jwt_payload, done) => {
+  const user = jwt_payload.sub;
+  return done(null, user);
+});
