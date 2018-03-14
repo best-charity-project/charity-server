cbeb0ad8ec618ba77ee012cb9620d72d63e97b0d
Aliaksei Sauran <a.sauran@godeltech.com>
2018-03-14 17:57:46 +0300

    Add strategy for signing user in

---
+++ passport/strategies/local-signup.js
@@ -0,0 +1,35 @@
+const passport = require('passport');
+const userAPI = require('../../models/users/userAPI');
+const LocalStrategy = require('passport-local').Strategy;
+const generateToken = require('./generateToken');
+
+module.exports = new LocalStrategy(
+  {
+    usernameField: 'email',
+    passwordField: 'password',
+    session: false,
+    passReqToCallback: true,
+  },
+  (req, email, password, done) => {
+    const userData = {
+      email: email.trim(),
+      password: password.trim(),
+      name: req.body.name.trim(),
+      country: req.body.country.trim(),
+      city: req.body.city.trim(),
+      reasonForRegistration: req.body.reasonForRegistration.trim(),
+    };
+    userAPI
+      .register(userData)
+      .then(userInfo => {
+        const payload = {
+          sub: userInfo,
+        };
+        const token = generateToken(payload);
+        return done(null, token, userInfo);
+      })
+      .catch(err => {
+        return done(err);
+      });
+  },
+);
