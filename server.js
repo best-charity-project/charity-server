b1004530b7bf3e862ba5377405e1e5446b6f2e0b
Aliaksei Sauran <a.sauran@godeltech.com>
2018-03-14 18:00:57 +0300

    Passport initialization

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
-const app = express();
+let app = express();
 const bodyParser = require('body-parser');
 const router = require('./routes/Routes');
+const passport = require('passport');
 const connectToDatabase = require('./database/database');
+const configurePassport = require('./passport/configurePassport');
+
 connectToDatabase();
 
 app.use(
@@ -11,7 +14,10 @@ app.use(
     extended: true,
   }),
 );
+
 app.use(bodyParser.json());
+
+app = configurePassport(app);
 
 const port = process.env.PORT || 8080;
 
