30777874012e4736549fa70a09d6a17b0d9b4c26
Aliaksei Sauran <a.sauran@godeltech.com>
2018-03-15 14:31:59 +0300

    Replace with very very basic email validation

--- passport/strategies/isValidEmail.js
+++ passport/strategies/isValidEmail.js
@@ -1,5 +1,2 @@
 module.exports = email =>
-  email.length < 255 &&
-  /^[A-Z0-9][A-Z0-9._%+-]{0,63}@(?:[A-Z0-9-]{1,63}\.){1,8}[A-Z]{2,63}$/i.test(
-    email,
-  );
+  email.length < 255 && /^\S+@\S+[\.][0-9a-z]+$/i.test(email);
