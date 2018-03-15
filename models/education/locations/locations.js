4bce6c9d7f765aa0fff31cdaafed75561d88d5d2
Aliaksei Husak <a.husak@godeltech.com>
2018-03-15 14:41:38 +0300

    removed _id from locations.js

--- models/education/locations/locations.js
+++ models/education/locations/locations.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const locationsSchema = new Schema({
   district: { type: Array, required: true },
   name: { type: String, required: true },
-  _id: { type: String, required: true },
 });
 
 const Locations = mongoose.model('regions', locationsSchema);
