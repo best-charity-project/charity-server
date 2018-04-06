854861dcbf6aef392a795f4b8c74644774894b2c
Aliaksei Sauran <a.sauran@godeltech.com>
2018-04-06 11:34:58 +0300

    Gather all account routes

--- routes/account/accountRoutes.js
+++ routes/account/accountRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 let router = express.Router();
 const changePassword = require('./changePassword');
+const changeForgottenPassword = require('./changeForgottenPassword');
 const restorePassword = require('./restorePassword');
+const validateToken = require('./validateToken');
 
 router = changePassword(router);
+router = changeForgottenPassword(router);
 router = restorePassword(router);
+router = validateToken(router);
 
 module.exports = router;
