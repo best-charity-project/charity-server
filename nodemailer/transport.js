7c33aeb3538ff6be41fe71c4074ba4d6975f4100
Aliaksei Sauran <a.sauran@godeltech.com>
2018-04-09 18:57:50 +0300

    Add env for jwtSecret and mailService

--- nodemailer/transport.js
+++ nodemailer/transport.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const { mailService, user, pass } = require('../configs/nodemailerConfig.json');
 
 module.exports = nodemailer.createTransport({
-  mailService: mailService || process.env.mailService,
+  service: mailService || process.env.mailService,
   auth: {
     user: user || process.env.user,
     pass: pass || process.env.pass,
