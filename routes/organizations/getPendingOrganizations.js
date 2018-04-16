dbb62e7eac9fc51773d492cdee04000be8e02b6d
Olga Yakimovich <v.yakimovich@godeltech.com>
2018-04-16 13:48:55 +0300

    added passport for getting pending items

--- routes/organizations/getPendingOrganizations.js
+++ routes/organizations/getPendingOrganizations.js
@@ -1,9 +1,14 @@
 const {
   getPendingOrganizations,
 } = require('../../models/organizations/organizationsAPI');
+const passport = require('passport');
+const isAdmin = require('../../middlewares/isAdmin');
 
 module.exports = router => {
-  router.route('/pending').get((req, res) => {
+  router.route('/pending').get(
+    passport.authenticate('jwt-auth', { session: false }),
+    isAdmin, 
+    (req, res) => {
     getPendingOrganizations(req.query)
       .then(items => {
         res.json(items);
