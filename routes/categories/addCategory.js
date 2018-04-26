7b6bb290409ea3a8a53a0aaf1129019e0d20c863
Aliaksei Sauran <a.sauran@godeltech.com>
2018-04-26 15:59:57 +0300

    Add category to the database

--- routes/categories/addCategory.js
+++ routes/categories/addCategory.js
@@ -1,12 +1,26 @@
 const { addCategory } = require('../../models/categories/categoriesAPI');
+const passport = require('passport');
+const isAdmin = require('../../middlewares/isAdmin');
 
 module.exports = router => {
-  router.route('/').post((req, res) => {
-    addCategory(req.body).then(() => {
-      res.json({
-        message: 'Category was created successfully!',
-      });
-    });
-  });
+  router
+    .route('/')
+    .post(
+      passport.authenticate('jwt-auth', { session: false }),
+      isAdmin,
+      (req, res) => {
+        addCategory(req.body)
+          .then(() =>
+            res.json({
+              message: 'Категория была успешно создана',
+            }),
+          )
+          .catch(err => {
+            res.status(500).json({
+              message: 'Запрос не может быть выполнен. Повторите попытку позже',
+            });
+          });
+      },
+    );
   return router;
 };
