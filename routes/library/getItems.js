59b20306148537671c0b02d3d186a580b1e6dcd6
Aliaksei Sauran <a.sauran@godeltech.com>
2018-05-04 14:41:44 +0300

    Move validation logic to route controller that handles task of getting items

--- routes/library/getItems.js
+++ routes/library/getItems.js
@@ -1,13 +1,22 @@
 const { getItems } = require('../../models/libraryItems/libraryItemAPI');
+const isValidLibraryQuery = require('../../models/utils/isValidLibraryQuery');
 
 module.exports = router => {
   router.route('/').get((req, res) => {
+    const { categoryTag, type } = req.query;
+    if (!isValidLibraryQuery({ categoryTag, type })) {
+      return res.status(400).json({
+        message: 'Проверьте правильность введенных данных',
+      });
+    }
     getItems(req.query)
       .then(items => {
         res.json(items);
       })
       .catch(err => {
-        res.status(400).json(err.message);
+        res.status(500).json({
+          message: 'Запрос не может быть выполнен. Повторите попытку позже',
+        });
       });
   });
   return router;
