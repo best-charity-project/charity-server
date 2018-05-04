e2f427946f924b2902821fc9bf90a3d21100ac9f
Aliaksei Sauran <a.sauran@godeltech.com>
2018-05-04 14:42:34 +0300

    Move validation logic to route controller that handles items counting

--- routes/library/getLibraryItemsAmount.js
+++ routes/library/getLibraryItemsAmount.js
@@ -1,13 +1,27 @@
 const { getItemsAmount } = require('../../models/libraryItems/libraryItemAPI');
+const isValidLibraryQuery = require('../../models/utils/isValidLibraryQuery');
 
 module.exports = router => {
   router.route('/count').get((req, res) => {
+    const { categoryTag, type } = req.query;
+    const query = {};
+    query.categoryTag = categoryTag;
+    if (type) {
+      query.type = type;
+    }
+    if (!isValidLibraryQuery(query)) {
+      return res.status(400).json({
+        message: 'Проверьте правильность введенных данных',
+      });
+    }
     getItemsAmount(req.query)
       .then(amount => {
         res.json(amount);
       })
-      .catch(err => {
-        res.status(400).json(err.message);
+      .catch(() => {
+        res.status(500).json({
+          message: 'Запрос не может быть выполнен. Повторите попытку позже',
+        });
       });
   });
   return router;
