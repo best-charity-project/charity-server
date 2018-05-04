97c319bc3232efcd1c22fa3b73a916095da35c28
Aliaksei Sauran <a.sauran@godeltech.com>
2018-05-04 14:37:23 +0300

    Create route to delete items of a certain category

---
+++ routes/library/deleteItems.js
@@ -0,0 +1,25 @@
+const { deleteItems } = require('../../models/libraryItems/libraryItemAPI');
+const isValidLibraryQuery = require('../../models/utils/isValidLibraryQuery');
+
+module.exports = router => {
+  router.route('/').delete((req, res) => {
+    const { categoryTag } = req.query;
+    if (!isValidLibraryQuery({ categoryTag })) {
+      return res.status(400).json({
+        message: 'Проверьте правильность введенных данных',
+      });
+    }
+    deleteItems({ categoryTag })
+      .then(items => {
+        res.json({
+          message: 'Документы были успешно удалены',
+        });
+      })
+      .catch(() => {
+        res.status(500).json({
+          message: 'Запрос не может быть выполнен. Повторите попытку позже',
+        });
+      });
+  });
+  return router;
+};
