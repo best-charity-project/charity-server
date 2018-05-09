07e102ec4d2440a1f9676459b6976a42e54aac94
Aleksei Savran <aleks171@hotmail.com>
2018-05-09 22:28:32 +0300

    Implement getting event by id

--- routes/events/eventsRoutes.js
+++ routes/events/eventsRoutes.js
@@ -3,8 +3,10 @@ let router = express.Router();
 
 const addEvents = require('./addEvents');
 const getEvents = require('./getEvents');
+const getEventById = require('./getEventById');
 
 router = addEvents(router);
 router = getEvents(router);
+router = getEventById(router);
 
 module.exports = router;
