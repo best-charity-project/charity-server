5af9e9cd1c0880a686d1d494bdb993d77f40651a
Karyna <mmf.vanyukki@gmail.com>
2018-05-27 17:59:45 +0300

    rename getEvent

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -14,7 +14,7 @@ module.exports = {
         async deleteEvent(req, res) {
             let eventDelete = await EventsModel.findByIdAndRemove(req.body);
             },
-        async getEvent(req, res) {
+        async getEvents(req, res) {
             let eventsList = await EventsModel.find();
             res.status(200).json({
                 events:eventsList
--- routes/event.routes.js
+++ routes/event.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller = require('../controllers/event.controller');
 
 router.post('/', controller.newEvent);
-router.get('/', controller.getEvent);
+router.get('/', controller.getEvents);
 router.delete('/', controller.deleteEvent);
 module.exports = router;
