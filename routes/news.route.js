95b1b9f56b3f4fe92cbeed91a29c0b0122b068c4
Valeria Koliagina <milleriaim@gmail.com>
2018-06-15 12:37:59 +0300

    big images done

--- routes/news.route.js
+++ routes/news.route.js
@@ -2,7 +2,9 @@ const express = require('express');
 const news = require('../controllers/news.controller');
 const router = express.Router();
 const multer = require('multer'); 
-const upload = multer();
+const upload = multer({
+    limits: { fieldSize: 25 * 1024 * 1024 }
+  });
 
 router.post('/', upload.array(), news.createNews);
 router.get('/', news.getNews);
