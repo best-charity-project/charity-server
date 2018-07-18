de4f1300d3c64421c5c2332c1e942fcf66893a6c
frikeykir <kiryltsytsynov@gmail.com>
2018-07-18 10:59:42 +0300

    add startline of prod

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     secret: 'mysecret',
     mongoose: {
-         uri: process.env.MONGO_URI || process.env.NODE_ENV == 'production' ? 'mongodb://charity:charity_godel717@ds241121.mlab.com:41121/charity-database':'mongodb://vadim:qwerty@ds219100.mlab.com:19100/charity-project',
+         uri: process.env.NODE_ENV == 'production' ? 'mongodb://charity:charity_godel717@ds241121.mlab.com:41121/charity-database':'mongodb://vadim:qwerty@ds219100.mlab.com:19100/charity-project',
     },
     crypto: {
         hash: {
