da0247396685ed6bb3036e91fb5ed7735a5601a8
frikeykir <kiryltsytsynov@gmail.com>
2018-07-23 13:36:58 +0300

    bugfix

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 global.env = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'production';
 let app = express();
 
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -21,7 +21,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'https://charity-test-app.herokuapp.com/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', /* 'X-Requested-With,content-type' */'Origin, X-Requested-With, Content-Type, Accept');
-    // res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 app.use(errorHandler);
