72ea0b83b2fe2db7207bf32244f24a93d619c4d4
frikeykir <kiryltsytsynov@gmail.com>
2018-07-23 11:11:24 +0300

    add fix in app.js wid access-control-allow-origin

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({
 
 app.use(passportMW.initialize());
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://charity-test-app.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://charity-test-app.herokuapp.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
