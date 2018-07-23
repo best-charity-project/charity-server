3ffc82deffcf8ba99a03d3de86a95cdd2d1a5af8
frikeykir <kiryltsytsynov@gmail.com>
2018-07-23 13:10:11 +0300

    comment credential string in app.js

--- app.js
+++ app.js
@@ -3,22 +3,23 @@ const logger = require('./utils/logger.utils');
 const errorHandler = require('./handlers/error.handlers');
 const passportMW = require('./utils/passport');
 const api = require('./routes');
-const cors = require('cors');
+const cors = require('cors')
 
 global.env = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'production';
 let app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }))
 
-app.use(cors());
+
 app.use(passportMW.initialize());
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', /* 'X-Requested-With,content-type' */'Origin, X-Requested-With, Content-Type, Accept');
-    // res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 app.use(errorHandler);
